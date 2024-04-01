fbb35a651844e70e632ab4696698de683775e9ae
snltty <1069410172@qq.com>
2024-04-01 15:58:11 +0800

    桌面共享

--- cmonitor/plugins/notify/report/NotifyWindows.cs
+++ cmonitor/plugins/notify/report/NotifyWindows.cs
@@ -4,13 +4,24 @@ namespace cmonitor.plugins.notify.report
 {
     public sealed class NotifyWindows : INotify
     {
+        public NotifyWindows()
+        {
+            AppDomain.CurrentDomain.ProcessExit += (sender, e) =>
+            {
+                CommandHelper.Windows(string.Empty, new string[] { "taskkill /f /im \"notify.win.exe\"" }, true);
+            };
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                CommandHelper.Windows(string.Empty, new string[] { "taskkill /f /im \"notify.win.exe\"" }, true);
+            };
+        }
         public void Update(NotifyInfo notify)
         {
             Task.Run(() =>
             {
                 CommandHelper.Windows(string.Empty, new string[] {
                         $"start cmonitor.notify.win.exe {notify.Speed} \"{notify.Msg}\" {notify.Star1} {notify.Star2} {notify.Star3}"
-                    },false);
+                    }, false);
             });
         }
     }
