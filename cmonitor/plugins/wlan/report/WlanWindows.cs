3ed4fec4fa70821896a2dd95545f9cc3ca29d190
snltty <1069410172@qq.com>
2024-04-21 16:51:13 +0800

    飞行模式

--- cmonitor/plugins/wlan/report/WlanWindows.cs
+++ cmonitor/plugins/wlan/report/WlanWindows.cs
@@ -11,35 +11,12 @@ namespace cmonitor.plugins.wlan.report
 
         public async Task<bool> WlanConnect(string name)
         {
-            try
-            {
-                var targetInterface = NativeWifi.EnumerateInterfaces()
-                 .FirstOrDefault(x =>
-                 {
-                     var radioSet = NativeWifi.GetInterfaceRadio(x.Id)?.RadioSets.FirstOrDefault();
-                     if (radioSet is null)
-                         return false;
-
-                     if (!radioSet.HardwareOn.GetValueOrDefault()) // Hardware radio state is off.
-                         return false;
-
-                     return (radioSet.SoftwareOn == false); // Software radio state is off.
-                 });
-
-                if (targetInterface != null)
-                {
-                    NativeWifi.TurnOnInterfaceRadio(targetInterface.Id);
-                }
-            }
-            catch (Exception)
-            {
-            }
-
             var wifi = NativeWifi.EnumerateAvailableNetworks().FirstOrDefault(c => c.ProfileName == name);
             if (wifi == null)
             {
                 return false;
             }
+            NativeWifi.TurnOnInterfaceRadio(wifi.Interface.Id);
             return await NativeWifi.ConnectNetworkAsync(wifi.Interface.Id, wifi.ProfileName, wifi.BssType, TimeSpan.FromSeconds(5));
         }
 
