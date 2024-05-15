b495772bff6a0a211f0005f1dde10f5fa640dc52
snltty <1069410172@qq.com>
2024-05-15 15:21:18 +0800

    发布模式

--- publish.bat
+++ publish.bat
@@ -7,8 +7,8 @@ mkdir public\\publish-zip
 for %%r in (win-x64,win-arm64,linux-x64,linux-arm64,osx-x64,osx-arm64) do (
 	for %%c in (release,ReleaseMonitor,ReleaseNetwork) do (
 		
-		dotnet publish ./cmonitor -c release -f net8.0 -o ./public/publish/%%c/%%r/single  -r %%r -p:PublishTrimmed=true  -p:TrimMode=partial  --self-contained true -p:TieredPGO=true  -p:DebugType=none -p:DebugSymbols=false -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true -p:DebuggerSupport=false -p:EnableUnsafeBinaryFormatterSerialization=false -p:EnableUnsafeUTF7Encoding=false -p:HttpActivityPropagationSupport=false -p:InvariantGlobalization=true  -p:MetadataUpdaterSupport=false  -p:UseSystemResourceKeys=true
-		dotnet publish ./cmonitor -c release -f net8.0 -r %%r -o ./public/publish/%%c/%%r/any/  -p:PublishSingleFile=true --self-contained false
+		dotnet publish ./cmonitor -c %%c -f net8.0 -o ./public/publish/%%c/%%r/single  -r %%r -p:PublishTrimmed=true  -p:TrimMode=partial  --self-contained true -p:TieredPGO=true  -p:DebugType=none -p:DebugSymbols=false -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true -p:DebuggerSupport=false -p:EnableUnsafeBinaryFormatterSerialization=false -p:EnableUnsafeUTF7Encoding=false -p:HttpActivityPropagationSupport=false -p:InvariantGlobalization=true  -p:MetadataUpdaterSupport=false  -p:UseSystemResourceKeys=true
+		dotnet publish ./cmonitor -c %%c -f net8.0 -r %%r -o ./public/publish/%%c/%%r/any/  -p:PublishSingleFile=true --self-contained false
 
 		echo F|xcopy "public\\extends\\%%c\\%%r\\*" "public\\publish\\%%c\\%%r\\single\\*"  /s /f /h /y
 		echo F|xcopy "public\\extends\\%%c\\%%r\\*" "public\\publish\\%%c\\%%r\\any\\*"  /s /f /h /y	
