b7fcf6ff666836c42821f118f5a00dbf211b3238
snltty <1069410172@qq.com>
2024-06-23 19:24:02 +0800

    rename

--- publish.bat
+++ publish.bat
@@ -13,22 +13,22 @@ cd ../
 
 for %%r in (win-x64,win-arm64) do (
 	dotnet publish ./linker.service -c release -f net8.0 -o public/extends/%%r/  -r %%r -p:PublishTrimmed=true  -p:TrimMode=partial  --self-contained true -p:TieredPGO=true  -p:DebugType=none -p:DebugSymbols=false  -p:EnableCompressionInSingleFile=true -p:DebuggerSupport=false -p:EnableUnsafeBinaryFormatterSerialization=false -p:EnableUnsafeUTF7Encoding=false -p:HttpActivityPropagationSupport=false -p:InvariantGlobalization=true  -p:MetadataUpdaterSupport=false  -p:UseSystemResourceKeys=true
-	echo F|xcopy "linker.tray.win\\dist\\*" "public\\extends\\%%r\\*"  /s /f /h /y
-	echo F|xcopy "linker\\msquic.dll" "public\\publish\\%%r\\msquic.dll"  /s /f /h /y
-	echo F|xcopy "linker\\msquic-openssl.dll" "public\\publish\\%%r\\msquic-openssl.dll"  /s /f /h /y
+	echo F|xcopy "linker.tray.win\\dist\\*" "public\\extends\\%%r\\linker-%%r\\*"  /s /f /h /y
+	echo F|xcopy "linker\\msquic.dll" "public\\extends\\%%r\\linker-%%r\\msquic.dll"  /s /f /h /y
+	echo F|xcopy "linker\\msquic-openssl.dll" "public\\extends\\%%r\\linker-%%r\\msquic-openssl.dll"  /s /f /h /y
 )
 for %%r in (win-x64,win-arm64,linux-x64,linux-arm64,osx-x64,osx-arm64) do (
-	echo F|xcopy "linker\\plugins\\tuntap\\tun2socks-%%r" "public\\extends\\%%r\\plugins\\tuntap\\tun2socks"  /s /f /h /y
+	echo F|xcopy "linker\\plugins\\tuntap\\tun2socks-%%r" "public\\extends\\%%r\\linker-%%r\\plugins\\tuntap\\tun2socks"  /s /f /h /y
 )
 
 
 
 for %%r in (win-x64,win-arm64,linux-x64,linux-arm64,osx-x64,osx-arm64) do (
 	
-	dotnet publish ./linker -c release -f net8.0 -o ./public/publish/%%r  -r %%r -p:PublishTrimmed=true  -p:TrimMode=partial  --self-contained true -p:TieredPGO=true  -p:DebugType=none -p:DebugSymbols=false -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true -p:DebuggerSupport=false -p:EnableUnsafeBinaryFormatterSerialization=false -p:EnableUnsafeUTF7Encoding=false -p:HttpActivityPropagationSupport=false -p:InvariantGlobalization=true  -p:MetadataUpdaterSupport=false  -p:UseSystemResourceKeys=true
-	echo F|xcopy "public\\extends\\%%r\\*" "public\\publish\\%%r\\*"  /s /f /h /y
+	dotnet publish ./linker -c release -f net8.0 -o ./public/publish/%%r/linker-%%r  -r %%r -p:PublishTrimmed=true  -p:TrimMode=partial  --self-contained true -p:TieredPGO=true  -p:DebugType=none -p:DebugSymbols=false -p:PublishSingleFile=true -p:EnableCompressionInSingleFile=true -p:DebuggerSupport=false -p:EnableUnsafeBinaryFormatterSerialization=false -p:EnableUnsafeUTF7Encoding=false -p:HttpActivityPropagationSupport=false -p:InvariantGlobalization=true  -p:MetadataUpdaterSupport=false  -p:UseSystemResourceKeys=true
+	echo F|xcopy "public\\extends\\%%r\\linker-%%r\\*" "public\\publish\\%%r\\linker-%%r\\*"  /s /f /h /y
 
-	echo F|xcopy "public\\extends\\any\\*" "public\\publish\\%%r\\*"  /s /f /h /y
+	echo F|xcopy "public\\extends\\any\\*" "public\\publish\\%%r\\linker-%%r\\*"  /s /f /h /y
 
-	7z a -tzip ./public/publish-zip/%%r.zip ./public/publish/%%r/*
+	7z a -tzip ./public/publish-zip/linker-%%r.zip ./public/publish/%%r/*
 )
