78a54df05d600e29df36cafc4dd3f74c9161bd09
snltty <1069410172@qq.com>
2024-07-18 16:00:55 +0800

    update

--- push-master.bat
+++ push-master.bat
@@ -4,5 +4,5 @@ git checkout master
 git reset --hard origin-github/dev
 git pull origin-github
 git add .
-git push origin-github master --tags
+git push origin-github master --tags 
 git checkout dev
