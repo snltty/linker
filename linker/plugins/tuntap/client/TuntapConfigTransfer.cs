7896b658ba32373d31b5b90caaf2130a07de2ae9
snltty <1069410172@qq.com>
2024-10-23 10:45:16 +0800

    改了很多东西

--- linker/plugins/tuntap/client/TuntapConfigTransfer.cs
+++ linker/plugins/tuntap/client/TuntapConfigTransfer.cs
@@ -254,9 +254,8 @@ namespace linker.plugins.tuntap.client
         {
             if (runningConfig.Data.Tuntap.LanIPs.Length == 0 && clientSignInState.Connection.LocalAddress.Address.Equals(clientSignInState.Connection.Address.Address) == false)
             {
-                
-                //runningConfig.Data.Tuntap.LanIPs = [clientSignInState.Connection.LocalAddress.Address];
-                //runningConfig.Data.Tuntap.Masks = [24];
+                runningConfig.Data.Tuntap.LanIPs = [clientSignInState.Connection.LocalAddress.Address];
+                runningConfig.Data.Tuntap.Masks = [24];
             }
             TuntapInfo info = new TuntapInfo
             {
