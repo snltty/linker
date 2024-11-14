da4a9ef1e814efd5da449d53404a3943eb8fb723
snltty <1069410172@qq.com>
2024-11-14 16:14:03 +0800

    网卡端口转发检测

--- linker/store/Storefactory.cs
+++ linker/store/Storefactory.cs
@@ -1,6 +1,7 @@
 ﻿using linker.libs;
 using linker.libs.extends;
 using LiteDB;
+using LiteDB.Engine;
 using System.Net;
 
 namespace linker.store
@@ -19,7 +20,29 @@ namespace linker.store
             bsonMapper.RegisterType<IPAddress>(serialize: (a) => a.ToString(), deserialize: (a) => IPAddress.Parse(a.AsString));
             bsonMapper.RegisterType<IPAddress[]>(serialize: (a) => a.ToJson(), deserialize: (a) => a.AsString.DeJson<IPAddress[]>());
 
-            database = new LiteDatabase(new ConnectionString($"Filename=./configs/db.db; Password={Helper.GlobalString}"), bsonMapper);
+            if (Encoded() == false)
+            {
+                database = new LiteDatabase(@"./configs/db.db", bsonMapper);
+                var rebuildOptions = new RebuildOptions { Password = Helper.GlobalString };
+                database.Rebuild(rebuildOptions);
+            }
+            else
+            {
+                database = new LiteDatabase(new ConnectionString($"Filename=./configs/db.db; Password={Helper.GlobalString}"), bsonMapper);
+            }
+        }
+
+        private bool Encoded()
+        {
+            try
+            {
+                string text = File.ReadAllText("./configs/db.db");
+                return text.Contains("This is a LiteDB file") == false;
+            }
+            catch (Exception)
+            {
+            }
+            return false;
         }
 
         public ILiteCollection<T> GetCollection<T>(string name)
@@ -32,4 +55,3 @@ namespace linker.store
             database.Checkpoint();
         }
     }
-}
