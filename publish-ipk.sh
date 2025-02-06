87ad7400e551eeae239e1b46bd9523b005b2a848
snltty <1069410172@qq.com>
2025-02-06 19:00:33 +0800

    测试

--- publish-ipk.sh
+++ publish-ipk.sh
@@ -9,15 +9,16 @@ cd ../../
 
 for r in ${rs[@]} 
 do
-    dotnet publish ./src/linker -c release -f net8.0 -o ./public/publish-ipk/${r}/linker -r linux-musl-${r}  -p:PublishSingleFile=true  --self-contained true  -p:TrimMode=partial -p:TieredPGO=true  -p:DebugType=none -p:EventSourceSupport=false -p:DebugSymbols=false -p:EnableCompressionInSingleFile=true -p:DebuggerSupport=false -p:EnableUnsafeBinaryFormatterSerialization=false -p:EnableUnsafeUTF7Encoding=false -p:HttpActivityPropagationSupport=false -p:InvariantGlobalization=true  -p:MetadataUpdaterSupport=false  -p:UseSystemResourceKeys=true -p:MetricsSupport=false -p:StackTraceSupport=false -p:XmlResolverIsNetworkingEnabledByDefault=false
-    cp -rf public/extends/any/web public/publish-ipk/${r}/linker/web
-    mkdir -p public/publish-ipk/${r}/linker/configs
-    mkdir -p public/publish-ipk/${r}/linker/logs
-    cp -rf src/linker/libmsquic-musl-${r}.so public/publish-ipk/${r}/linker/libmsquic.so
+    dotnet publish ./src/linker -c release -f net8.0 -o ./public/publish/${r} -r linux-musl-${r}  -p:PublishSingleFile=true  --self-contained true  -p:TrimMode=partial -p:TieredPGO=true  -p:DebugType=none -p:EventSourceSupport=false -p:DebugSymbols=false -p:EnableCompressionInSingleFile=true -p:DebuggerSupport=false -p:EnableUnsafeBinaryFormatterSerialization=false -p:EnableUnsafeUTF7Encoding=false -p:HttpActivityPropagationSupport=false -p:InvariantGlobalization=true  -p:MetadataUpdaterSupport=false  -p:UseSystemResourceKeys=true -p:MetricsSupport=false -p:StackTraceSupport=false -p:XmlResolverIsNetworkingEnabledByDefault=false
+    cp -rf public/extends/any/web public/publish/${r}/web
+    mkdir -p public/publish/${r}/configs
+    mkdir -p public/publish/${r}/logs
+    cp -rf src/linker/libmsquic-musl-${r}.so public/publish/${r}/libmsquic.so
 
-    cp -rf ipk public/publish-ipk/${r}
+    mkdir -p public/publish-ipk/${r}
+    cp -rf ipk/* public/publish-ipk/${r}/
     mkdir -p public/publish-ipk/${r}/data/usr/bin/linker
-    cp -rf public/publish-ipk/${r} public/publish-ipk/${r}/data/usr/bin/linker
+    cp -rf public/publish/${r}/* public/publish-ipk/${r}/data/usr/bin/linker/
     sed -i "s|{version}|1.6.6|g" public/publish-ipk/${r}/control/control
     chmod +x public/publish-ipk/${r}/data/etc/init.d/linker
     chmod +x public/publish-ipk/${r}/control/control
@@ -36,7 +37,5 @@ do
     tar -czvf control.tar.gz -C control .
     echo "2.0" > debian-binary
     ar r linker-${r}.ipk debian-binary data.tar.gz control.tar.gz
-    ls 
-    pwd
     cd ../../../
 done
