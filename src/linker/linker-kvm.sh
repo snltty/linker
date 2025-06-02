b2871c78ac92e6134e20700994f5206e79925044
snltty <1069410172@qq.com>
2025-06-02 19:15:35 +0800

    kvm

--- src/linker/linker-kvm.sh
+++ src/linker/linker-kvm.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
 if [ ! -f /etc/kvmd/.init_flag ]; then
-	cat >> /etc/systemd/system/linker.service  << EOF
-[Unit]
-Description=linker
+    cat >> /etc/kvmd/supervisord.conf << EOF
 
-[Service]
-WorkingDirectory=/linker
-ExecStartPre=/bin/chmod +x /linker/linker
-ExecStart=/linker/linker
-ExecStop=/bin/kill $MAINPID
-ExecReload=/bin/kill -HUP $MAINPID
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
+[program:linker]
+command=./linker
+directory=/linker/
+autostart=true
+autorestart=true
+priority=12
+stopasgroup=true
+stdout_logfile=/linker/stdout
+stdout_logfile_maxbytes = 0
+redirect_stderr=true
 EOF
-
-    systemctl daemon-reload >/dev/null 2>&1
-    systemctl enable linker >/dev/null 2>&1
-    systemctl restart linker >/dev/null 2>&1
 fi
-
 
 /kvmd/init.sh
