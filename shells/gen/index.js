9db48c9f5ef0dfac3d6535e49d9ae060c8e2d5eb
snltty <iswwping@163.com>
2025-11-18 11:47:45 +0800

    196

--- shells/gen/index.js
+++ shells/gen/index.js
@@ -220,7 +220,7 @@ function writeUploadIpk(data, tagName) {
             },
             with: {
                 'upload_url': '${{ steps.get_release.outputs.upload_url }}',
-                'asset_path': `./public/publish-fpk/docker/inker-docker-${arch}.fpk`,
+                'asset_path': `./public/publish-fpk/docker/linker-docker-${arch}.fpk`,
                 'asset_name': `linker-docker-${arch}.fpk`,
                 'asset_content_type': 'application/fpk'
             }
