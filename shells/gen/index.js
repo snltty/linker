1a73895e850ff811c541fe5a1fb8f65c2885c080
snltty <iswwping@163.com>
2026-03-07 18:16:09 +0800

    1.9.91

--- shells/gen/index.js
+++ shells/gen/index.js
@@ -115,7 +115,7 @@ function writeUpload(data, tagName) {
         }
     });
     
-    /*
+    
     data.jobs.build.steps.push({
         name: `upload-version-oss`,
         id: `upload-version-oss`,
@@ -129,7 +129,7 @@ function writeUpload(data, tagName) {
             'target-path': `/downloads/linker/version.txt`
         }
     });
-    */
+    
     
     data.jobs.build.steps.push({
         name: `upload-install-service-oss`,
