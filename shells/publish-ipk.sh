137b8cc289cac390927d5484c05f248d14055726
snltty <iswwping@163.com>
2026-05-23 15:33:36 +0800

    apk

--- shells/publish-ipk.sh
+++ shells/publish-ipk.sh
@@ -49,10 +49,27 @@ do
     mkdir -p public/publish-apk/${r}/data/usr/bin/linker
     cp -rf public/publish/${r}/* public/publish-apk/${r}/data/usr/bin/linker/
 
-    sed -i "s|{version}|1.9.99|g" public/publish-apk/${r}/control/package.info
-    sed -i "s|{apk_arch}|noarch|g" public/publish-apk/${r}/control/package.info
+    apk_info_file=public/publish-apk/${r}/control/package.info
+    if [ ! -f "${apk_info_file}" ] && [ -f public/publish-apk/${r}/control/.PKGINFO ]; then
+        awk -F ' = ' '
+            /^#/ || NF < 2 { next }
+            $1 == "pkgname" { print "name:" $2; next }
+            $1 == "pkgver" { print "version:" $2; next }
+            $1 == "pkgdesc" { print "description:" $2; next }
+            $1 == "arch" { print "arch:" $2; next }
+            $1 == "license" { print "license:" $2; next }
+            $1 == "origin" { print "origin:" $2; next }
+            $1 == "url" { print "url:" $2; next }
+            $1 == "maintainer" { print "maintainer:" $2; next }
+            $1 == "depend" { deps = deps ? deps " " $2 : $2; next }
+            END { if (deps) print "depends:" deps }
+        ' public/publish-apk/${r}/control/.PKGINFO > "${apk_info_file}"
+    fi
+
+    sed -i "s|{version}|1.9.99|g" "${apk_info_file}"
+    sed -i "s|{apk_arch}|noarch|g" "${apk_info_file}"
     sed -i 's/\r$//' public/publish-apk/${r}/data/etc/init.d/linker
-    sed -i 's/\r$//' public/publish-apk/${r}/control/package.info
+    sed -i 's/\r$//' "${apk_info_file}"
     sed -i 's/\r$//' public/publish-apk/${r}/control/.post-install
     sed -i 's/\r$//' public/publish-apk/${r}/control/.pre-deinstall
 
@@ -67,7 +84,23 @@ do
 
     apk_info_args=()
     while IFS= read -r line; do
-        [ -n "${line}" ] && apk_info_args+=(--info "${line}")
+        line=${line%%#*}
+        line=$(printf '%s' "${line}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+        [ -z "${line}" ] && continue
+
+        if printf '%s' "${line}" | grep -q '^[^:=][^:=]*[[:space:]]*=[[:space:]]*'; then
+            key=$(printf '%s' "${line}" | sed 's/[[:space:]]*=.*//')
+            value=$(printf '%s' "${line}" | sed 's/^[^=]*=[[:space:]]*//')
+            case "${key}" in
+                pkgname) key=name ;;
+                pkgver) key=version ;;
+                pkgdesc) key=description ;;
+                depend) key=depends ;;
+            esac
+            line="${key}:${value}"
+        fi
+
+        apk_info_args+=(--info "${line}")
     done < control/package.info
 
     "${APK_CMD}" mkpkg "${apk_info_args[@]}" \
