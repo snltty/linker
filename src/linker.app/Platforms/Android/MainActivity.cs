dcbf44246ddd17a4fd4e0981c9c6e6a5bcbd6841
snltty <iswwping@163.com>
2026-06-17 23:34:45 +0800

    mesh

--- src/linker.app/Platforms/Android/MainActivity.cs
+++ src/linker.app/Platforms/Android/MainActivity.cs
@@ -125,11 +125,12 @@ namespace linker.app
             //tuntapDecenter.Refresh();
             await ValueTask.CompletedTask.ConfigureAwait(false);
         }
-        public async ValueTask Receive(ITunnelConnection connection, ReadOnlyMemory<byte> buffer)
+        public ValueTask<bool> Receive(ITunnelConnection connection, ReadOnlyMemory<byte> buffer)
         {
-           await tuntapTransfer.Write(connection.RemoteMachineId, buffer).ConfigureAwait(false);
+           return tuntapTransfer.Write(connection.RemoteMachineId, buffer);
         }
 
+       
     }
 
     /// <summary>
